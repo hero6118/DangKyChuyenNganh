3f0f9457c60c231638a0746e0873cfa2b1633e5f
quocduyy <nguyenquocduy1010.ltp@gmail.com>
2023-03-08 09:25:51 +0700

    duy

--- Controllers/MajorController.php
+++ Controllers/MajorController.php
@@ -23,4 +23,22 @@ class MajorController
             header("Location: index.php");
         }
     }
+    function edit(){
+        if($_SERVER['REQUEST_METHOD'] == 'GET'){
+            $id = $_GET['id'];
+            $student = $this->model->getById($id);
+            require_once("Views/Edit.php");
+        }
+    }
+    function update(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $id =$_POST['id'];
+            $mssv =$_POST['StudentId'];
+            $hoten =$_POST['name'];
+            $lop =$_POST['class'];
+            $chuyennganh =$_POST['major'];
+            $this->model->addStudent($mssv,$hoten,$lop,$chuyennganh);
+            header("Location: index.php");
+        }
+    }
 }
--- Models/Registration.php
+++ Models/Registration.php
@@ -16,4 +16,28 @@ class Registration
             ));
         }
     }
+    public function getById($id){
+        $sql = 'SELECT * FROM phieudangky WHERE MaPhieu = :id';
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(array(
+            ':id' == $id
+        ));
+        $result =  $stmt->fetch(PDO::FETCH_ASSOC)
+        return $result;
+    }
+    
+    function updateStudent($id,$hoten,$lop,$chuyennganh,$mssv){
+        $sql = "UPDATE phieudangky SET Lop = :lop, Major =:chuyennganh, Hoten = :hoten, MSSV = :mssv WHERE Maphieu = :id;";
+        $Stmt = $this ->db->prepare($sql);
+        $stmt->execute(array(
+            ':id' == $id,
+            ':lop' == $lop,
+            ':chuyennganh' == $chuyennganh,
+            ':hoten' == $hoten,
+            ':mssv' == $mssv
+        ));
+        $result =  $stmt->fetch(PDO::FETCH_ASSOC)
+        return $result;
+    }
+
 }

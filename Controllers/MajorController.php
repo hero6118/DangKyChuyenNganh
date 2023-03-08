7cc96297ede4fce9f3c140f163a7f4b1db5c6a87
quocduyy <nguyenquocduy1010.ltp@gmail.com>
2023-03-08 09:04:46 +0700

    duy

--- Controllers/MajorController.php
+++ Controllers/MajorController.php
@@ -1,8 +1,26 @@
 <?php
+require_once("Config/Database.php");
+require_once("Models/Registration.php");
 class MajorController 
 {
+    private $model;
+    public function __construct()
+    {
+        global $pdo;
+        $this->model = new Registration($pdo);
+    }
     function index()
     {
         require_once("Views/Register.php");
+    }
+    function add(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $hoten = $_POST['name'];
+            $lop = $_POST['class'];
+            $chuyennganh = $_POST['major'];
+            $mssv = $_POST['StudentID'];
+            $this->model->addStudent($mssv,$hoten,$lop,$chuyennganh);
+            header("Location: index.php");
+        }
     }
 }

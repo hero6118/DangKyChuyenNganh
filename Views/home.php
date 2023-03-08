ad10fd32cdbbe586e06a175d4a003cd0a24a45eb
quocduyy <nguyenquocduy1010.ltp@gmail.com>
2023-03-08 08:34:00 +0700

    duy

---
+++ Views/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Chu</title>
+   
+</head>
+<body>
+    <header class="bg-primary text-while py-3">
+        <div class="container d-flex justify-content-between align-items-center">
+            <h1 class="mb-0">Dang Ky Chuyen Nganh</h1>
+            <nav>
+                <a href="?r=register" class="text-while py-3">Dang Ky</a>
+                <a href="?r=list" class="text-while py-3">Xem danh sach dang ky</a>
+            </nav>
+
+        </div>
+        
+        
+
+
+    </header>
+</body>
+</html>
